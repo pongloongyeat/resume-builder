de6b865fa3f2fd3492c9673d50414b8ef488af4a
pongloongyeat <pongloongyeat@gmail.com>
2020-12-17 04:50:46 +0800

    Add register form

--- account/views.py
+++ account/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .forms import RegisterForm
 
 # Create your views here.
 def login_view(request):
@@ -6,7 +7,18 @@ def login_view(request):
 
     return render(request, 'account/login.html', context)
 
-def register_view(request):
-    context = {}
+def register_view(response):
+    if response.method == "POST":
+        form = RegisterForm(response.POST)
 
-    return render(request, 'account/register.html', context)
+        if form.is_valid():
+            form.save()
+
+    else:
+        form = RegisterForm()
+
+    context = {
+        "form": form
+    }
+
+    return render(response, 'account/register.html', context)

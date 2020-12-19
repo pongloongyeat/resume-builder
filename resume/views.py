4b3f424833ec214475f0c491e39ecea940cbd36d
pongloongyeat <pongloongyeat@gmail.com>
2020-12-19 12:59:26 +0800

    Resume: Render accordian based on $collapse_id

--- resume/views.py
+++ resume/views.py
@@ -11,20 +11,28 @@ def dashboard_view(request):
 
 @login_required(login_url='login')
 def create_view(response):
+    collapse_id = 4
+    education_forms = []
+
     if response.method == "POST":
+
         personal_form = PersonalDetailsForm(data=response.POST)
-        education_form = EducationForm(data=response.POST)
+
+        print(response.POST)
 
         if personal_form.is_valid() and education_form.is_valid():
             pass
 
     else:
         personal_form = PersonalDetailsForm
-        education_form = EducationForm
+
+        for i in range(0, collapse_id):
+            education_forms.append(EducationForm)
 
     context = {
         "personal_form": personal_form,
-        "education_form": education_form
+        "education_forms": education_forms,
+        "collapse_id": collapse_id,
     }
 
     return render(response, 'resume/create.html', context)

fc965fdc8ab70617e40990d3d9840263902fd8aa
pongloongyeat <pongloongyeat@gmail.com>
2020-12-21 03:59:30 +0800

    Use inlineformset

--- resume/views.py
+++ resume/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.forms import inlineformset_factory
 from .forms import PersonalDetailsForm, EducationForm
-from .models import EducationDetails
+from .models import Resume, EducationDetails
 
 # Create your views here.
 @login_required(login_url='login')
@@ -21,8 +22,6 @@ def edit_vew(response, pk):
     personal_details = resume.personaldetails
     education_details_all = resume.educationdetails_set.all()
 
-    number_of_education_forms = len(education_details_all)
-
     if response.method == "POST":
         if response.POST.get("new_education"):
             pass
@@ -35,43 +34,34 @@ def edit_vew(response, pk):
             'about_me': personal_details.about_me,
         })
 
-        education_forms = [EducationForm(initial={
-            'institution': education_details_all[i].institution,
-            'course': education_details_all[i].course,
-            'start_date': education_details_all[i].start_date,
-            'end_date': education_details_all[i].end_date,
-            'country': education_details_all[i].country,
-        }) for i in range(0, number_of_education_forms)]
+        education_formset = get_education_formset(resume, extra=0)
 
     context = {
         "personal_form": personal_form,
-        "education_forms": education_forms,
+        "education_formset": education_formset,
     }
 
     return render(response, 'resume/create.html', context)
 
-def fill_education_form_list(data, count):
-    education_forms = []
+def get_education_formset (user_resume_model, extra):
+    EducationFormset = inlineformset_factory(Resume, EducationDetails, extra=extra, fields=(
+        'institution',
+        'course',
+        'start_date',
+        'end_date',
+        'country',
+        'description'
+    ))
 
-    institution_list    = data.getlist('institution')
-    course_list         = data.getlist('course')
-    start_date_0_list   = data.getlist('start_date_0')
-    start_date_1_list   = data.getlist('start_date_1')
-    end_date_0_list     = data.getlist('end_date_0')
-    end_date_1_list     = data.getlist('end_date_1')
-    country_list        = data.getlist('country')
+    education_formset = EducationFormset(instance=user_resume_model)
 
-    for i in range(0, count):
-        data = {
-            'institution': institution_list[i],
-            'course': course_list[i],
-            'start_date_0': start_date_0_list[i],
-            'start_date_1': start_date_1_list[i],
-            'end_date_0': end_date_0_list[i],
-            'end_date_1': end_date_1_list[i],
-            'country': country_list[i]
-        }
+    # Somehow crispy tags aren't applying
+    # Bootstrap styling onto the inline
+    # form so this is a workaround :/
+    for education_form in education_formset:
+        for field in education_form.fields:
+            education_form.fields[field].widget.attrs.update({
+                'class': 'form-control'
+            })
 
-        education_forms.append(EducationForm(data))
-
-    return education_forms
+    return education_formset

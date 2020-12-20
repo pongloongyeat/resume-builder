87e4b640da6a94649fd7c8e88ac9e0ab8313a860
pongloongyeat <pongloongyeat@gmail.com>
2020-12-21 04:04:42 +0800

    Remove unused EducationForm

--- resume/forms.py
+++ resume/forms.py
@@ -32,38 +32,3 @@ class PersonalDetailsForm(forms.Form):
     class Meta:
         model = PersonalDetails
         fields = ["first_name", "last_name", "email_address", "phone", "about_me"]
-
-class EducationForm(forms.Form):
-    institution = forms.CharField(max_length=50, required=False)
-    course      = forms.CharField(max_length=75, required=False)
-    start_date  = MonthField(required=False)
-    end_date    = MonthField(required=False)
-    country     = forms.CharField(max_length=60, required=False) # Longest belongs to UK with 56 chars!
-    description = forms.CharField(widget=forms.Textarea(), max_length=300, required=False)
-
-    # Stying-related
-    institution.widget.attrs.update({
-        "class": "form-control"
-    })
-    course.widget.attrs.update({
-        "class": "form-control"
-    })
-    start_date.widget.attrs.update({
-        "class": "form-control",
-        "placeholder": "YYYY"
-    })
-    end_date.widget.attrs.update({
-        "class": "form-control",
-        "placeholder": "YYYY"
-    })
-    country.widget.attrs.update({
-        "class": "form-control",
-    })
-    description.widget.attrs.update({
-        "class": "form-control",
-        "rows": 4
-    })
-
-    class Meta:
-        model = EducationDetails
-        fields = ["institution", "course", "start_date", "end_date", "country", "description"]

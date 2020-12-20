9f1739219c549a0d03727439c4fd460adf124e83
pongloongyeat <pongloongyeat@gmail.com>
2020-12-21 05:29:49 +0800

    Add styling to "About me" field

--- resume/forms.py
+++ resume/forms.py
@@ -6,3 +6,6 @@ class PersonalDetailsForm(forms.ModelForm):
     class Meta:
         model = PersonalDetails
         fields = ['first_name', 'last_name', 'email_address', 'phone', 'about_me']
+        widgets = {
+            'about_me': forms.Textarea(attrs={'rows': 4}),
+        }

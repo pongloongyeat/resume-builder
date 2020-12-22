1fd31903b93c6210eb34e11a96208e9179ecf60b
pongloongyeat <pongloongyeat@gmail.com>
2020-12-23 06:12:16 +0800

    Add work details model

--- resume/models.py
+++ resume/models.py
@@ -35,3 +35,16 @@ class EducationDetails(models.Model):
 
     def __str__(self):
         return self.institution
+
+class WorkDetails(models.Model):
+    resume = models.ForeignKey(Resume, on_delete=models.CASCADE)
+
+    position        = models.CharField(max_length=50, blank=True)
+    company_name    = models.CharField(max_length=75, blank=True)
+    start_date      = MonthField(blank=True)
+    end_date        = MonthField(blank=True)
+    country         = models.CharField(max_length=60, blank=True) # Longest belongs to UK with 56 chars!
+    description     = models.CharField(max_length=300, blank=True)
+
+    def __str__(self):
+        return ("%s, %s" % (self.position, self.company_name))
